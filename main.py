70ccda177f84389a9d67e25bd0adb8fbb6861cd6
Ben8120 <chuabenshern5@gmail.com>
2022-07-15 20:23:39 +0800

    sum

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 def test():
     print("Test")
 
+def sum():
+    x = 2
+    y = 3
+    print("Sum of x + y is: ")
+    print(x+y)
+
 def main():
     print("Hello World!")
     test()
+    sum()
 
 if __name__ == "__main__":
     main()
