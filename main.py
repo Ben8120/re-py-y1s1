16e0d5a539d3334ac961ec19188210c0242e0848
Ben8120 <chuabenshern5@gmail.com>
2022-07-17 21:03:47 +0800

    basic operation

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+from audioop import mul
+
+
 def test():
     print("Test")
 
-def sum():
-    x = 2
-    y = 3
+def sum(n1, n2):
     print("Sum of x + y is: ")
-    print(x+y)
+    print(n1 + n2)
+
+def minus(n1, n2):
+    print("Minus of x and y is: ")
+    print(n1 - n2)
+
+def multiply(n1, n2):
+    print("Multiplication of both numbers are: ")
+    print(n1 * n2)
+
+def division(n1, n2):
+    print("Division of both numbers are: ")
+    print(n1 / n2)
 
 def main():
-    print("Hello World!")
-    test()
-    sum()
+    division(5,7)
 
 if __name__ == "__main__":
     main()
