d3fe1ab796b0274038cee3b5345b9bfdcb51744e
Ben8120 <chuabenshern5@gmail.com>
2022-07-18 12:43:13 +0800

    fstring for all basic operator

--- main.py
+++ main.py
@@ -5,24 +5,22 @@ def test():
     print("Test")
 
 def sum(n1, n2):
-    print(f"Sum of x + y is: {n1+n2}")
+    print(f"Sum of both numbers are: {n1+n2}")
 
 def minus(n1, n2):
-    print("Minus of x and y is: ")
-    print(n1 - n2)
+    print(f"Minus of both numbers are: {n1-n2}")
 
 def multiply(n1, n2):
-    print("Multiplication of both numbers are: ")
-    print(n1 * n2)
+    print(f"Multiplication of both numbers are: {n1*n2}")
 
 def division(n1, n2):
-    print("Division of both numbers are: ")
-    print(n1 / n2)
+    print(f"Division of both numbers are: {n1/n2}")
 
 def main():
-    #what = "Ben"
-    #print(f'Hello {what}')
     sum(5,7)
+    minus(7,5)
+    multiply(5,5)
+    division(5,5)
 
 if __name__ == "__main__":
     main()
